3b16c73d70806b03f5b090cadecb6f3996cfc6c4
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-01 12:28:12 +0100

    ajouter entity MouvementStock

--- src/main/java/com/tricol/tricolV2/entity/MouvementStock.java
+++ src/main/java/com/tricol/tricolV2/entity/MouvementStock.java
@@ -1,30 +1,52 @@
 package com.tricol.tricolV2.entity;
 
-
 import com.tricol.tricolV2.entity.enums.TypeMouvement;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MouvementStock {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
+    @NotNull(message = "La date du mouvement est obligatoire")
+    @PastOrPresent(message = "La date du mouvement doit être dans le passé ou aujourd’hui")
     private LocalDateTime dateMouvement;
-    private TypeMouvement type ;
+
+    @NotNull(message = "Le type de mouvement est obligatoire")
+    @Enumerated(EnumType.STRING)
+    private TypeMouvement type;
+
+    @NotNull(message = "La quantité est obligatoire")
+    @DecimalMin(value = "0.0", inclusive = false, message = "La quantité doit être supérieure à zéro")
     private BigDecimal quantite;
+
+    @NotNull(message = "Le coût unitaire est obligatoire")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Le coût unitaire doit être supérieur à zéro")
     private BigDecimal coutUnitaire;
 
     @ManyToOne
-    @JoinColumn(name = "produit_id")
+    @JoinColumn(name = "produit_id", nullable = false)
+    @NotNull(message = "Le produit est obligatoire")
     private Produit produit;
 
     @ManyToOne
     @JoinColumn(name = "commande_id")
     private CommandeFournisseur commande;
-    private String commentaire ;
+
+    @Size(max = 255, message = "Le commentaire ne doit pas dépasser 255 caractères")
+    private String commentaire;
 }
