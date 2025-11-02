2a3b40436292c79cdc2baf5802e3759d3b72705a
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-02 13:17:59 +0100

    j'ai ajouter produit mapper

--- src/main/java/com/tricol/tricolV2/mapper/ProduitMapper.java
+++ src/main/java/com/tricol/tricolV2/mapper/ProduitMapper.java
@@ -1,4 +1,20 @@
 package com.tricol.tricolV2.mapper;
 
+import com.tricol.tricolV2.dto.ProduitDTO;
+import com.tricol.tricolV2.entity.Produit;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface ProduitMapper {
+
+    @Mapping(target = "mouvements", ignore = true)
+    @Mapping(target = "lignesCommande", ignore = true)
+    Produit toEntity(ProduitDTO dto);
+
+    ProduitDTO toDTO(Produit produit);
+
+    List<ProduitDTO> toDTOs(List<Produit> produits);
 }
