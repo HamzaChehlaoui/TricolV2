d8d99be725b13ced42246fa90dc420a5cc775550
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-05 10:08:58 +0100

    j'ai ajoute documentation de l'api (Ãswager)

--- src/main/java/com/tricol/tricolV2/config/SwaggerConfig.java
+++ src/main/java/com/tricol/tricolV2/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package com.tricol.tricolV2.config;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tricol API Documentation")
+                        .description("Documentation de l'API de gestion des fournisseurs et des commandes")
+                        .version("2.0.0"));
+    }
 }
