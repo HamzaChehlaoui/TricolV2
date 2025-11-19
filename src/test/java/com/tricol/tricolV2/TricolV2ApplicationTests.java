2f26d28aaf3dba601515a4b79441fd82e121e817
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-19 19:35:26 +0100

    change Testcontener to H2

--- src/test/java/com/tricol/tricolV2/TricolV2ApplicationTests.java
+++ src/test/java/com/tricol/tricolV2/TricolV2ApplicationTests.java
@@ -2,9 +2,10 @@ package com.tricol.tricolV2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.tricol.tricolV2.config.AbstractIntegrationTest;
 
 @SpringBootTest
-class TricolV2ApplicationTests {
+class TricolV2ApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
--- src/test/java/com/tricol/tricolV2/config/AbstractIntegrationTest.java
+++ src/test/java/com/tricol/tricolV2/config/AbstractIntegrationTest.java
@@ -1,39 +1,18 @@
 package com.tricol.tricolV2.config;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public abstract class AbstractIntegrationTest {
-
-    @Container
-    public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
-            .withDatabaseName("tricol_test")
-            .withUsername("test")
-            .withPassword("test");
-
-    @BeforeAll
-    static void startContainer() {
-        mysql.start();
-    }
-
-    @AfterAll
-    static void stopContainer() {
-        mysql.stop();
-    }
 
     @DynamicPropertySource
     static void registerProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
-        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+        registry.add("spring.datasource.url", () -> "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MYSQL;DATABASE_TO_UPPER=false");
+        registry.add("spring.datasource.username", () -> "sa");
+        registry.add("spring.datasource.password", () -> "");
+        registry.add("spring.datasource.driver-class-name", () -> "org.h2.Driver");
         // Let Liquibase run against the container
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
+        registry.add("spring.liquibase.enabled", () -> "false");
     }
 }
